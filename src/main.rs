f15a106ee8612802163afa76153806c136286d06
macro <macro@world.ru>
2020-12-07 03:22:45 +0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,132 @@
+use std::{io, fs};
+use std::path::Path;
+use std::fs::File;
+use std::io::{Write, BufReader, Read};
+
+
+trait MenuAction {
+    fn get_title(&self) -> String;
+    fn invoke(&self);
+}
+
+struct MainMenu {
+    menu: Vec<Box<dyn MenuAction>>,
+
+}
+
+impl MainMenu {
+    fn print_menu(&self) {
+        for (index, menu) in self.menu.iter().enumerate() {
+            println!("{}) {}", index + 1, menu.get_title());
+        }
+    }
+
+    fn run(&self, option: usize) {
+        if option <= self.menu.len() && option > 0 {
+            self.menu.get(option - 1).expect("Выход за приделы вектора").invoke();
+        } else {
+            println!("Такого пункта нет в меню {}", option)
+        }
+    }
+}
+
+struct SecondMenu {
+    title: String,
+    action: fn(),
+}
+
+impl MenuAction for SecondMenu {
+    fn get_title(&self) -> String {
+        String::from(&self.title)
+    }
+
+    fn invoke(&self) {
+        (&self.action)();
+    }
+}
+
+
+fn get_input() -> String {
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_goes_into_input_above) => {}
+        Err(_no_updates_is_fine) => {}
+    }
+    input.trim().to_string()
+}
+
+fn main() {
+    let first_option = Box::new(SecondMenu {
+        title: "Указать пути до папки с \\\\wls$\\".to_string(),
+        action:
+        || write_in_file("core/path.cfg", "Введите путь: "),
+    });
+    let second_option = Box::new(SecondMenu {
+        title: "Создать проект".to_string(),
+        action:
+        || create_project(),
+    });
+    let all_menu: Vec<Box<dyn MenuAction>> = vec![first_option, second_option];
+    let m = MainMenu { menu: all_menu };
+
+    loop {
+        m.print_menu();
+        match get_input().parse::<usize>() {
+            Err(_) => println!("Вы должны ввести число !!!"),
+            Ok(option) => m.run(option)
+        }
+    }
+
+}
+
+fn write_in_file(dirs: &str, prompt: &str) {
+    let mut vec_dir: Vec<&str> = dirs.split("/").collect();
+    vec_dir.pop().unwrap();
+
+    fs::create_dir_all(vec_dir.join("/")).unwrap();
+    let path = Path::new(dirs);
+    let display = path.display();
+
+    // Open a file in write-only mode, returns `io::Result<File>`
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    loop {
+        println!("{}", prompt);
+        let input = get_input();
+        if Path::new(&input).is_dir() {
+            match file.write_all(input.as_bytes()) {
+                Err(why) => panic!("couldn't write to {}: {}", display, why),
+                Ok(_) => break println!("путь установлен"),
+            }
+        } else {
+            println!("Такого пути не существует: {}", input);
+        }
+    }
+}
+
+fn create_project() {
+    match fs::File::open("core/path.cfg") {
+        Err(_) => println!("Нужно объязательно создать путь!!"),
+        Ok(file) => {
+            let mut buf_reader = BufReader::new(file);
+            let mut contents = String::new();
+            buf_reader.read_to_string(&mut contents).unwrap();
+            loop {
+                println!("Введите путь где создать проект пример: D:\\my_project ");
+                let project_path = get_input();
+
+                let mut full_project_path: Vec<&str> = project_path.split("\\").collect();
+                let last_folder = full_project_path.pop().unwrap();
+                // let command_project_link = format!("mklink /D {} {}\\{}", project_path, contents, last_folder);
+                fs::create_dir_all(format!("{}/{}",contents, last_folder)).unwrap();
+                match std::os::windows::fs::symlink_dir(format!("{}/{}",contents,last_folder).as_str(), format!("{}",project_path).as_str()){
+                    Err(e) => println!("ОШИБКА ПОПРОБУЙТЕ ЗАПУСТИТЬ ПРОГРАММУ ОТ АДМИНИСТРАТОРА {}", e),
+                    Ok(_) => break println!("ПОЗДРАВЛЯЮ ПРОЕКТ СОЗДАН")
+                }
+            }
+        }
+    };
+}
